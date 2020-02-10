1204cf582c65dd174f7161a6a7c02940f1a243c1
X-Mix <prudnikoffvi@gmail.com>
2020-02-10 16:45:46 +0300

    feat: add on scroll animation for header

---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", function(){
+    let header = document.getElementsByClassName("header")[0];
+    window.addEventListener('scroll', function() {
+        //console.log(pageYOffset + 'px');
+        if (pageYOffset > 100){
+            header.classList.add("header-scroll");
+            //console.log(header);
+        }else{
+            header.classList.remove("header-scroll");
+        }
+    });
+});
