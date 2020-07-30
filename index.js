0d9a992d86143b1c7ddcba413a4bde3e760a04d6
Vladislav <PrudnikOFFVI@gmail.com>
2020-07-30 19:30:15 +0300

    feat: add preloader

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", function(){
+    const preloader = document.getElementsByClassName("preloader__wrapper")[0];
     const burger = document.getElementsByClassName('burger')[0];
     const header = document.getElementsByClassName("header")[0];
     const burgerIcon = document.getElementsByClassName("burger__icon")[0];
@@ -14,4 +15,7 @@ window.addEventListener("DOMContentLoaded", function(){
         burger.classList.toggle("burger-active");
         burgerIcon.classList.toggle("burger__icon-play_animation");
     });
+    window.addEventListener("load", () => {
+        preloader.classList.add("loaded");
+    })
 });
